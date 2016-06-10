f22c4637e5e4aec667a1763afd4ab60e333603ba
reshma <reshma@qburst.com>
2016-06-10 16:22:00 +0530

    reading json file using xmlhttprequest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.overrideMimeType("application/json");
+xmlhttp.open('GET', 'assets/js/gallery-item.json', true);
+xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == "200") {
+         	updateMe(JSON.parse(xmlhttp.responseText));
+          }
+    };
+xmlhttp.send(null);
 
-	function updateMe(){
+function updateMe(galleryArray){
 	var output='';
 	for (i in galleryArray) {
-        output += 	"<div class='responsive'><div class='img'>"
-        					+"<img src='"+galleryArray[i].src+"' alt='"+galleryArray[i].alt+"' title='"+galleryArray[i].title+"' width='300' height='200'>"
-        					+"<div style='display:none;'>"+galleryArray[i].desc+"</div>"
-        					+"<div class='title'>"+galleryArray[i].title+"</div>"
-        					+"</div></div>";
-    }
-    document.getElementById("banner-img-wrapper").innerHTML=output;
+	    output += 	"<div class='responsive'><div class='img'>"
+	     			+"<img src='"+galleryArray[i].src+"' alt='"+galleryArray[i].alt+"' title='"+galleryArray[i].title+"' width='300' height='200'>"
+	        		+"<div style='display:none;'>"+galleryArray[i].desc+"</div>"
+	        		+"<div class='title'>"+galleryArray[i].title+"</div>"
+	        		+"</div></div>";
+	        		document.getElementById("banner-img-wrapper").innerHTML = output;
+	}
 
 	var images = document.getElementsByTagName('img');
 	var modalImg = document.getElementById("img01");
@@ -16,14 +25,13 @@
 	var descText = document.getElementById("caption");
 	var i;
 	for (i = 0; i < images.length; i++) {
-	   images[i].onclick = function(){
-	   		modal.style.display = "block";
-	   		modalImg.src = this.src;
-	   		modalImg.alt = this.alt;
-	   		titleText.innerHTML = this.title;
-	   		descText.innerHTML = this.nextElementSibling.innerHTML;
+		   images[i].onclick = function(){
+		   		modal.style.display = "block";
+		   		modalImg.src = this.src;
+		   		modalImg.alt = this.alt;
+		   		titleText.innerHTML = this.title;
+		   		descText.innerHTML = this.nextElementSibling.innerHTML;
 		}
 	}
-  }
-
+}
 
